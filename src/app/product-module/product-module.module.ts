93dccbb59090132a82a36b0912d34102cfc258fa
AliaaHas <aliaahassan191@gmail.com>
2022-05-12 23:17:21 +0200

    userupdates

--- src/app/product-module/product-module.module.ts
+++ src/app/product-module/product-module.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { AddProductComponent } from './AddProduct/add-product/add-product.component';
  import { ProductDetailsComponent } from './ProductDetails/product-details/product-details.component';
 import { ProductsComponent } from './products/products.component';
 import { ShoppingCartComponent } from './shopping-cart/shopping-cart.component';
@@ -16,14 +15,14 @@ const routes: Routes = [
 
     {path:'Products', component:ShoppingCartComponent},
     {path:'Products/:ID',component:ProductDetailsComponent},
-    {path:'AddProduct',component:AddProductComponent },
+   
 
     ]
 
 
 @NgModule({
   declarations: [
-    AddProductComponent,
+
     ProductDetailsComponent,
     ProductsComponent,
     ShoppingCartComponent,
